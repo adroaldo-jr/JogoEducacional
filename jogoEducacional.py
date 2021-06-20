8fe0eae3dbd328f8ff3cbb851fc85d6c3d200500
adroaldo-jr <adroaldosouto@gmail.com>
2021-06-20 13:59:04 -0300

    Adicionado as letras ao jogoEducacional.py
    Adicionado vogais e consoantes e configurado a grade por onde passarão.

--- jogoEducacional.py
+++ jogoEducacional.py
@@ -1,9 +1,9 @@
 import pygame
 import random
-import string
 import time
 from funcoes import Logs
 
+# Recebe o nome e e-mail do jogador e salva no log
 '''nome = str(input('Digite o seu nome: ')).upper().strip()
 email = str(input('Digite seu e-mail: ')).upper().strip()
 Logs(nome, email)'''
@@ -27,7 +27,15 @@ fundo = pygame.image.load('assets/floresta.jpg')
 leao = pygame.image.load('assets/leao.png')
 icone = pygame.image.load("assets/lionIcon.png")
 
+def vogal():
+    vogais = ['A', 'E', 'I', 'O', 'U']
+    vogaisAleatoria = vogais[random.randint(0,4)]
+    return vogaisAleatoria
 
+def consoante():
+    consoantes = ['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'X', 'Z']
+    consoanteAleatoria = consoantes[random.randint(0, 18)]
+    return consoanteAleatoria
 
 def jogo():
     pygame.mixer.music.load('assets/intro.mp3')
@@ -36,6 +44,17 @@ def jogo():
     posicaoLeaoY = altura * 0.8
     movimentoX = 0
     movimentoY = 0
+    vogalPosicaoX = largura * 0.45
+    vogalPosicaoY = -30
+    consoantePosicaoX = 700
+    consoantePosicaoY = altura * 0.45
+    velocidade = 5
+    letraLargura = 75
+    letraAltura = 75
+    vogal()
+    font = pygame.font.SysFont(None, 54)
+    vogais = font.render(vogal(), True, black)
+    consoantes = font.render(consoante(), True, black)
 
     while True:
         for event in pygame.event.get():
@@ -58,6 +77,8 @@ def jogo():
         pygame.mixer.Sound.play(somIntro)
         display.fill(white)
         display.blit(fundo, (0,0))
+
+        # Pega posição do Leão e impede que cruze as bordas
         posicaoLeaoX = posicaoLeaoX + movimentoX
         if posicaoLeaoX < 0:
             posicaoLeaoX = 0
@@ -70,8 +91,23 @@ def jogo():
             posicaoLeaoY = 480
         
         display.blit(leao, (posicaoLeaoX, posicaoLeaoY))
+        display.blit(vogais, ((vogalPosicaoX, vogalPosicaoY)))
+        display.blit(consoantes, ((consoantePosicaoX, consoantePosicaoY)))
+        vogalPosicaoY = vogalPosicaoY + velocidade
+        consoantePosicaoX = consoantePosicaoX - velocidade
 
-        #display.blit(textoTela, (50, 700))
+        if vogalPosicaoY > altura:
+            vogal()
+            vogais = font.render(vogal(), True, black)
+            vogalPosicaoY = -30
+            vogalPosicaoX = random.randrange(0, largura-50)
+
+        if consoantePosicaoX < 0:
+            consoante()
+            consoantes = font.render(consoante(), True, black)
+            consoantePosicaoX = 800
+            consoantePosicaoY = random.randrange(0, altura-50)
+
         pygame.display.update()
         fps.tick(60)
 
